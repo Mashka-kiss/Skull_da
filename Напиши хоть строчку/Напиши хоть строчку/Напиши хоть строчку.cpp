d0949d4803f1635ded134cb869998fe9399dfff0
Mashka-kiss <131745710+Mashka-kiss@users.noreply.github.com>
2023-04-25 18:44:02 +0300

    Add files via upload

---
+++ Напиши хоть строчку/Напиши хоть строчку/Напиши хоть строчку.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    std::string slovo;
+    std::cout << "Введите слово: ";
+    std::cin >> slovo;
+    reverse(slovo.begin(), slovo.end());
+    std::cout << "Результат: " + slovo << std::endl;
+}
